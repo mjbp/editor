927c03f3eece71be8ce392a66cb86ba5a778fdd9
mjbp <mick@binaryvein.com>
2013-09-27 00:12:55 +0100

    fun with dom traversal and clean up

--- js/editor.js
+++ js/editor.js
@@ -104,40 +104,163 @@ function Editor(selector, opts) {
             }
             return this;
         },
-        chromeCleanUp : function () {
-            //check for and remove span in chrome ;_;
+        cleanUp : function (styleType) {
+            //needs attention
+            /*
+            the old way
+            //check for and remove span, br, empty elements and inline styles in chrome ;_;
             var self = this,
-                updatedNodes = self.findNodes(self.selection.focusNode);
+                updatedNodes = self.findNodes(self.selection.focusNode),
+                kids,
+                i,
+                l;
             
-            if (updatedNodes.SPAN) {
+            if (!!updatedNodes.SPAN) {
                 self.removeNode(updatedNodes.SPAN);
             }
+            if (styleType === "inline") {
+                kids = self.selection.focusNode.parentNode.childNodes;
+            } else {
+                kids = self.selection.focusNode.childNodes;
+            }
+            
+            l = kids.length;
+            for (i = 0; i < l; i += 1) {
+                //here be dragons - remove inline style added by chrome
+                if (kids[i].attributes) {
+                    if (kids[i].attributes.style) {
+                        kids[i].attributes.removeNamedItem('style');
+                    }
+                }
+                if (kids[i].nodeName === 'SPAN' || kids[i].nodeName === 'BR') {
+                    self.removeNode(kids[i]);
+                } else {
+                    //remove empty tags
+                    if (/^\s*$/.test(kids[i].innerHTML)) {
+                        self.removeNode(kids[i]);
+                    }
+                }
+            }
+            */
+            
+            /* remove all unwanted and empty nodes & attributes */
+            /* new way */
+            var self = this,
+                i,
+                l,
+                child,
+                disallowedEls = ['BR', 'SPAN'],
+                disallowedAttrs = ['class', 'style'],
+                j,
+                children,
+                k,
+                next,
+                removeBool = false,
+                walk = function (node, func) {
+                    children = node.childNodes;
+                    for (i = 0; i < children.length; i += 1) {
+                        func(children[i]);
+                        walk(children[i], func);
+                    }
+                };
+            
+            /******
+             * DOM TAVERSAL IS NOT WORK ;_; 
+             */
+            //console.log(this.liveElement);
+            walk(this.liveElement, function (node) {
+                if (node.nodeType === 1) {
+                    //remove unwanted attributes
+                    for (j = 0; j < disallowedAttrs.length; j += 1) {
+                        if (node.hasAttribute(disallowedAttrs[j])) {
+                            node.removeAttribute(disallowedAttrs[j]);
+                        }
+                    }
+                    //remove unwanted nodes
+                    for (k = 0; k < disallowedEls.length; k += 1) {
+                        if (disallowedEls[k] === node.nodeName) {
+                            removeBool = true;
+                        }
+                    }
+                    if (removeBool) {
+                        console.log('trying to remove...' + node);
+                        self.removeNode(node);
+                    }
+                }
+                if (!removeBool && /^\s*$/.test(node.innerHTML)) {
+                    self.removeNode(node);
+                }
+            });
+            /*
+            while (child) {
+                next = child.nextSibling;
+                nodeName = child.nodeName;
+                console.log(nodeName);
+                if (child.nodeType === 1) {
+                    //remove unwanted attributes
+                    for (j = 0; j < disallowedAttrs.length; j += 1) {
+                        if (child.hasAttribute(disallowedAttrs[j])) {
+                            child.removeAttribute(disallowedAttrs[j]);
+                        }
+                    }
+                }
+                
+                //remove unwanted nodes
+                for (k = 0; k < allowedEls.length; k += 1) {
+                    if (allowedEls[k] === nodeName) {
+                        removeBool = false;
+                    }
+                }
+                if (removeBool || /^\s*$/.test(child.innerHTML)) {
+                    console.log('trying to remove...');
+                    self.removeNode(child);
+                }
+                child = next;
+            }*/
             
             return self;
         },
+        incompatibleElements : {
+            'UL' : ['BLOCKQUOTE', 'P', 'H1', 'H2', 'H3', 'H4', 'H5', 'H6'],
+            'OL' : ['BLOCKQUOTE', 'P', 'H1', 'H2', 'H3', 'H4', 'H5', 'H6'],
+            'BLOCKQUOTE' : ['LI', 'OL', 'UL'],
+            'heading' : ['BLOCKQUOTE', 'LI', 'OL', 'UL']
+        },
         executeStyle : function (c) {
-            //test these commands
             var self = this,
+                styleType,
                 dispatchTable = {
-                    'bold' : function () { d.execCommand('bold', false); },
-                    'italic' : function () { d.execCommand('italic', false); },
+                    'bold' : function () {
+                        d.execCommand('bold', false);
+                        return;
+                    },
+                    'italic' : function () {
+                        d.execCommand('italic', false);
+                        return;
+                    },
                     'ul' : function () { dispatchTable.list('UL'); },
                     'ol' : function () { dispatchTable.list('OL'); },
                     'quote' : function () {
                         var parentNodes = self.findNodes(self.selection.focusNode),
-                            text;
+                            text,
+                            i,
+                            incompatibles = self.incompatibleElements.heading,
+                            l = incompatibles.length;
                         
-                        if (!!parentNodes.LI) {
-                            self.removeNode(parentNodes.LI);
-                            self.removeNode(parentNodes.UL) || self.removeNode(parentNodes.OL);
+                        //remove elements we don't want mixing...
+                        for (i = 0; i < l; i += 1) {
+                            if (!!parentNodes[incompatibles[i]]) {
+                                self.removeNode(parentNodes[incompatibles[i]]);
+                            }
                         }
                         if (!!parentNodes.BLOCKQUOTE) {
                             self.removeNode(parentNodes.BLOCKQUOTE);
-                            document.execCommand('formatBlock', false, 'p');
-			                document.execCommand('outdent');
+                            d.execCommand('formatBlock', false, 'p');
+			                d.execCommand('outdent');
                         } else {
-                            document.execCommand('formatBlock', false, 'blockquote');
+                            d.execCommand('formatBlock', false, 'blockquote');
                         }
+                        return;
                         
                         //mares - chrome removes p, 
                         //so either cannot support multi-line quotes or re-wrap in p
@@ -149,7 +272,18 @@ function Editor(selector, opts) {
                     'h5' : function () { dispatchTable.heading('H5'); },
                     'h6' : function () { dispatchTable.heading('H6'); },
                     'heading' : function (h) {
-                        var parentNodes = self.findNodes(self.selection.focusNode);
+                        var parentNodes = self.findNodes(self.selection.focusNode),
+                            i,
+                            incompatibles = self.incompatibleElements.heading,
+                            l = incompatibles.length;
+                        
+                        //remove elements we don't want mixing...
+                        //deja vu... refactor out
+                        for (i = 0; i < l; i += 1) {
+                            if (!!parentNodes[incompatibles[i]]) {
+                                self.removeNode(parentNodes[incompatibles[i]]);
+                            }
+                        }
                         
                         if (!!parentNodes[h]) {
                             d.execCommand('formatBlock', false, 'p');
@@ -157,13 +291,15 @@ function Editor(selector, opts) {
                         } else {
                             d.execCommand('formatBlock', false, h);
                         }
+                        
+                        return;
                     },
                     'list' : function (listType) {
                         var parentNodes = self.findNodes(self.selection.focusNode),
-                            updatedNodes;
-                        
-                        //self.prepare(listType);
-                        
+                            updatedNodes,
+                            i,
+                            incompatibles = self.incompatibleElements[listType],
+                            l = incompatibles.length;
                         
                         if (!!parentNodes[listType]) {
                             self.removeNode(parentNodes.LI);
@@ -171,11 +307,11 @@ function Editor(selector, opts) {
                             d.execCommand('formatBlock', false, 'p');
                             d.execCommand('outdent');
                         } else {
-                            if (!!parentNodes.BLOCKQUOTE) {
-                                self.removeNode(parentNodes.BLOCKQUOTE);
-                            }
-                            if (!!parentNodes.P) {
-                                self.removeNode(parentNodes.P);
+                            //remove elements we don't want mixing...
+                            for (i = 0; i < l; i += 1) {
+                                if (!!parentNodes[incompatibles[i]]) {
+                                    self.removeNode(parentNodes[incompatibles[i]]);
+                                }
                             }
                             if (listType === 'UL') {
                                 d.execCommand('insertunorderedlist', false);
@@ -183,31 +319,35 @@ function Editor(selector, opts) {
                                 d.execCommand('insertorderedlist', false);
                             }
                         }
+                        return;
                     }
                 };
             dispatchTable[c]();
-            self.chromeCleanUp();
+            self.cleanUp();
             
             //update UI button states
             //this.updateUI();
+            //reselect if seleciton has failed (see adding list style)
             //reposition UI
             this.placeUI();
             
             return this;
         },
         removeNode : function (node) {
-            var fragment = d.createDocumentFragment();
+            var self = this,
+                fragment = d.createDocumentFragment();
             
             while (node.firstChild) {
+                //console.log(node.firstChild.parentElement);
                 fragment.appendChild(node.firstChild);
             }
+            console.log('boom:' + node + '> ' + node.parentNode);
             node.parentNode.replaceChild(fragment, node);
             
-            return this;
+            return;
         },
         findNodes : function (element) {
             var nodeNames = {};
-            
             //recursion through node parents
             //array of nodes hierachy indexed by nodeName
             while (element.parentNode) {
@@ -230,6 +370,7 @@ function Editor(selector, opts) {
                 l = this.elements.length,
                 checkForHighlight = function () {
                     self.selection = w.getSelection();
+                    self.liveElement = this;
                     
                     //SELECTION NEEDS REFINING??
                     if (self.selection.isCollapsed === false) {
